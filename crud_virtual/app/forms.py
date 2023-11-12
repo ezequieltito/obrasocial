d5972bddc64b1643f820fc2fe70d0768ea38d7aa
MauroHoyos <mauroohoyos@gmail.com>
2023-11-12 01:26:59 -0300

    TipoDePlanes

--- crud_virtual/app/forms.py
+++ crud_virtual/app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User, Group
+from .models import Afiliados, TipoPlanes
 
 class CustomLoginForm(AuthenticationForm):
     # Puedes personalizar el formulario aquí si es necesario
@@ -15,23 +16,41 @@ class CustomLoginForm(AuthenticationForm):
     )
 
 class CustomUserCreationForm(UserCreationForm):
-	email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True)
+    # Agrega el campo de selección desde el modelo TipoPlanes
+    id_tipoPlan = forms.ModelChoiceField(queryset=TipoPlanes.objects.all(), required=False)
+    # Agrega el campo de entrada para el dni_afi
+    dni_afi = forms.IntegerField(required=False)
 
-	class Meta:
-		model = User
-		fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
-	def clean_email(self):
-		email = self.cleaned_data['email']
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
-		if User.objects.filter(email=email).exists():
-			raise forms.ValidationError('Este correo electrónico ya está registrado')
-		return email
-	
-	def save(self, commit=True):
-		user = super().save(commit=False)
-		user.save()        
-        # Una vez que el usuario se ha guardado en la base de datos, agrégalo al grupo "Usuario"
-		grupo_usuario, created = Group.objects.get_or_create(name='Usuario')
-		user.groups.add(grupo_usuario)
-		return user
-       
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Este correo electrónico ya está registrado')
+        return email
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.save()        
+        # Guarda el campo de selección en el modelo Afiliados
+        id_tipoPlan = self.cleaned_data.get('id_tipoPlan')
+        dni_afi = self.cleaned_data.get('dni_afi')
+        if id_tipoPlan:
+            afiliado = Afiliados.objects.create(
+                user=user,
+                id_tipoPlan=id_tipoPlan,
+                # Agrega otros campos según sea necesario
+                nombre_afi=user.first_name,
+                apellido_afi=user.last_name,
+                dni_afi=dni_afi,
+                user_id=user.id,
+            )
+
+        # Una vez que el usuario se ha guardado en la base de datos, agrégalo al grupo "Usuario" por defecto
+        grupo_usuario, created = Group.objects.get_or_create(name='Usuario')
+        user.groups.add(grupo_usuario)
+        return user
